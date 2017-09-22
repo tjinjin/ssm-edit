9a810b20bf55d73cb2f21682d176f409137e1328
tjinjin <tanaka.masato@classmethod.jp>
2017-09-22 22:10:11 +0900

    Improve

--- cli/ssm/session.go
+++ cli/ssm/session.go
@@ -1,26 +1,16 @@
 package cli
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
-func GetSession(profile string) {
+func GetSession(profile string) (svc *ssm.SSM) {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		Config:  aws.Config{Region: aws.String("ap-northeast-1")},
 		Profile: profile,
 	}))
-	svc := ssm.New(sess)
-	input := &ssm.DescribeParametersInput{}
-	resp, err := svc.DescribeParameters(input)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println(resp)
+	test := ssm.New(sess)
+	return test
 }
