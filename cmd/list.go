d318d562add13ce1a46a31846a4b08bd173c6552
tjinjin <tjinjinprogram@gmail.com>
2017-09-26 23:51:56 +0900

    minor cosmetic tweaks

--- cmd/list.go
+++ cmd/list.go
@@ -23,13 +23,8 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "list ssm parameters",
+	Long:  `list ssm parameters`,
 	Run: func(cmd *cobra.Command, args []string) {
 		p := viper.GetString("profile")
 		r := viper.GetString("region")
