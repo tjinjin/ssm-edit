00d45cb1f49591b8c1678dceb58b4a3f4e5a66e5
tjinjin <tjinjinprogram@gmail.com>
2018-05-08 23:20:01 +0900

    Implement sample prompt

--- cmd/tui.go
+++ cmd/tui.go
@@ -15,28 +15,25 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	tui "github.com/tjinjin/ssm-edit/cli"
 )
 
 // tuiCmd represents the tui command
 var tuiCmd = &cobra.Command{
 	Use:   "tui",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "use tui mode",
+	Long:  `use tui mode`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("tui called")
+		p := viper.GetString("profile")
+		r := viper.GetString("region")
+		tui.Tui(p, r)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(tuiCmd)
+	RootCmd.AddCommand(tuiCmd)
 
 	// Here you will define your flags and configuration settings.
 
